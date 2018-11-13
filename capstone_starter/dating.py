27fa788777cfc2b29695948cd86b9e4ed12c719d
Justin Haut <Justin.a.haut@gmail.com>
2018-11-12 22:16:15 -0600

    Codecademy generated from the project submission page.

--- capstone_starter/dating.py
+++ capstone_starter/dating.py
@@ -5,6 +5,8 @@ from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
 import timeit
 
 #Create your df here:
@@ -110,3 +112,79 @@ stop = timeit.default_timer()
 print('SVM Validation Accuracy:',classifier.score(validation_set[['smokes_vals','drinks_vals','drug_vals']],validation_set.religion))
 total += stop - start
 print('Time to run SVM:', total)
+
+
+#####Regression#####
+
+#get rid of -1s as answers.
+df_inc = df[df.income != -1]
+print(df_inc.income.value_counts())
+
+#get rid of NaNs in smokes and drinks columns
+df_inc = df_inc.dropna(subset=['drinks', 'smokes'])
+
+#plot income vs smoking
+plt.scatter(df_inc.income, df_inc.smokes, alpha = 0.1)
+plt.xlabel('Income')
+plt.ylabel('Smokes')
+plt.title('Income vs Smoking')
+plt.show()
+
+#plot income vs drinking
+plt.scatter(df_inc.income, df_inc.drinks, alpha = 0.1)
+plt.xlabel('Income')
+plt.ylabel('Drinks')
+plt.title('Income vs Drinking')
+plt.show()
+
+x = df_inc[['drinks_vals','drug_vals']]
+y = df_inc.income
+
+start = timeit.default_timer()
+#####Multiple Linear Regression#####
+x_train, x_test, y_train, y_test = train_test_split(x,y, train_size = 0.8, test_size = 0.2, random_state = 1)
+incMLR = LinearRegression()
+incMLR.fit(x_train, y_train)
+stop = timeit.default_timer()
+y_predict = incMLR.predict(x_test)
+
+print('Time to run MLR:', stop - start)
+print('MLR Score:', incMLR.score(x_train, y_train))
+print('MLR Score:', incMLR.score(x_test, y_test))
+print('MLR Coefficients:', incMLR.coef_)
+
+plt.scatter(y_test, y_predict)
+plt.xlabel('Actual Income')
+plt.ylabel('Predicted Income')
+plt.title('Actual vs. Predicted Income')
+plt.plot(range(200000),range(200000))
+plt.show()
+
+#####KNN Regressor#####
+KNN_scores = []
+for n in range(100, 200):
+    incRegressor = KNeighborsRegressor(n_neighbors = n, weights = 'distance')
+    incRegressor.fit(x,y)
+    KNN_scores.append(incRegressor.score(x,y))
+
+#time running of KNN regressor
+total = 0
+start = timeit.default_timer()
+incRegressor = KNeighborsRegressor(n_neighbors = 104, weights = 'distance')
+incRegressor.fit(x,y)
+stop = timeit.default_timer()
+
+print('Time to run KNN Regressor:', stop - start)
+print('KNN Regressor Score:', incRegressor.score(x,y))
+
+#plot KNN Regressor Scores to find best N
+plt.plot(range(100,200), KNN_scores)
+plt.xlabel('Neighbors')
+plt.ylabel('Score')
+plt.title('Neighbors vs Model Score')
+plt.show()
+
+#barchart groupings
+#inc_smk_drnk = df_inc.groupby(['income', 'smokes', 'drinks']).size().unstack()
+#inc_smk_drnk.plot(kind='barh',stacked=True, figsize=[20,20])
+
